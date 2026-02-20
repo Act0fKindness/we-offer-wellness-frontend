5cb47e4c11f5fd0b4ff43be86e03d54fb35b4829
Daniel Harding <daniel.robert.harding@gmail.com>
2026-02-20 22:09:06 +0000

    Back to working version.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,8 +4,20 @@
 
 @include('home.sections.hero')
 
-@include('home.sections.mindful_times_ribbon')
-
+<section data-v-f43bb09d="" class="mindful-times-ribbon" aria-label="Mindful Times update">
+    <div data-v-f43bb09d="" class="container">
+        <div data-v-f43bb09d="" class="ribbon-inner"><span data-v-f43bb09d="" class="ribbon-label">Mindful Times</span><span
+            data-v-f43bb09d="">New stories, guides and practitioner advice every week.</span>
+            <div data-v-f43bb09d="" class="ribbon-links"><a data-v-f43bb09d=""
+                                                            href="https://times.weofferwellness.co.uk"
+                                                            class="link-wow">Read the latest</a><span
+                data-v-f43bb09d="" aria-hidden="true">•</span><a data-v-f43bb09d=""
+                                                                 href="https://times.weofferwellness.co.uk/seeking-wellness"
+                                                                 class="link-wow">Listen to the Podcast</a>
+            </div>
+        </div>
+    </div>
+</section>
 <section data-v-f43bb09d="" class="py-4">
     <div data-v-f43bb09d="" class="container">
         <div data-v-f43bb09d="" class="wow-ultra">
