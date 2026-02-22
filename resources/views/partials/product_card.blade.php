fb669945ff829fc1ae5994ce25f5fdb967f08a47
Daniel Harding <daniel.robert.harding@gmail.com>
2026-02-22 18:55:11 +0000

    Product card: show globe for Online and pin for physical locations under stars (14px icons)

--- resources/views/partials/product_card.blade.php
+++ resources/views/partials/product_card.blade.php
@@ -297,9 +297,29 @@
               <span>(231)</span>
             </div>
           <div class="meta">
-            @if($durationLabel)<span class="item">{{ $durationLabel }}</span>@endif
-            @if($locationLabel)<span class="item">{{ $locationLabel }}</span>@endif
-            @if($nextLabel)<span class="item">Next: {{ $nextLabel }}</span>@endif
+            @if($durationLabel)
+              <span class="item">{{ $durationLabel }}</span>
+            @endif
+            @if($locationLabel)
+              <span class="item">
+                @if($isOnline && count($physical)===0)
+                  <!-- Globe icon for Online -->
+                  <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M4.37 7.657c2.063.528 2.396 2.806 3.202 3.87 1.07 1.413 2.075 1.228 3.192 2.644 1.805 2.289 1.312 5.705 1.312 6.705M20 15h-1a4 4 0 0 0-4 4v1M8.587 3.992c0 .822.112 1.886 1.515 2.58 1.402.693 2.918.351 2.918 2.334 0 .276 0 2.008 1.972 2.008 2.026.031 2.026-1.678 2.026-2.008 0-.65.527-.9 1.177-.9H20M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                  </svg>
+                @else
+                  <!-- Pin icon for physical locations -->
+                  <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"/>
+                  </svg>
+                @endif
+                {{ $locationLabel }}
+              </span>
+            @endif
+            @if($nextLabel)
+              <span class="item">Next: {{ $nextLabel }}</span>
+            @endif
           </div>
 
         </div>
