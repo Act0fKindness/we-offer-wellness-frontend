0c9e9a5ba017aae65843166033bc934b3721f8e1
Daniel Harding <daniel.robert.harding@gmail.com>
2026-02-22 01:36:35 +0000

    Details/About: render HTML by decoding entities in About section (no escaping)

--- resources/views/offering/partials/sections/about.blade.php
+++ resources/views/offering/partials/sections/about.blade.php
@@ -2,7 +2,7 @@
 @if($body !== '')
   <h3 class="wow-section-title mt-3">About this experience</h3>
   <div class="wow-about" id="wowAbout">
-    <div class="wow-paragraph wow-clamp" data-lines="3">{!! $body !!}</div>
+    <div class="wow-paragraph wow-clamp" data-lines="3">{!! html_entity_decode($body) !!}</div>
     <a href="#" class="wow-readmore" data-wow-readmore data-target="#wowAbout">Read More</a>
   </div>
 @endif
