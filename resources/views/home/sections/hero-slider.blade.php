d1dd7d02fb0e3f9720a9ee0b5a765900f590be66
Daniel Harding <daniel.robert.harding@gmail.com>
2026-02-24 23:08:50 +0000

    Adding nav bar pages.

--- resources/views/home/sections/hero-slider.blade.php
+++ resources/views/home/sections/hero-slider.blade.php
@@ -276,9 +276,6 @@
 <div class="swiper wow-hero-swiper" data-hero-swiper>
   <div class="swiper-wrapper">
     <div class="swiper-slide">
-      @include('home.sections.hero-slider-2')
-    </div>
-    <div class="swiper-slide">
       @include('home.sections.hero-slider-1')
     </div>
   </div>
