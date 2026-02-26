3a7f71cc539bafb166d9be637ab9f5a20a7f8251
Daniel Harding <hello@weofferwellness.co.uk>
2026-02-26 23:01:39 +0000

    fix: restore standalone auth head markup

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    @php($pageTitle = trim($__env->yieldContent('page-title', 'We Offer Wellness™')))
-    @include('partials.head')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('page-title', 'We Offer Wellness™')</title>
+    <link rel="icon" type="image/png" href="https://testing.studio.weofferwellness.co.uk/workspace-favicon.png?v=2">
+    <link rel="shortcut icon" href="https://testing.studio.weofferwellness.co.uk/workspace-favicon.png?v=2">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
