76cfe2c4b6ec065c937daadb12b110f7b5d011f7
Daniel Harding <hello@weofferwellness.co.uk>
2026-03-03 12:52:08 +0000

    feat: update offering feature badges and location text

--- resources/views/offering/partials/sections/features.blade.php
+++ resources/views/offering/partials/sections/features.blade.php
@@ -1,6 +1,14 @@
 @php
-  $locs = $locationsList ?? [];
-  $locCount = is_array($locs) ? count($locs) : 0;
+  $locs = is_array($locationsList ?? null) ? array_values(array_filter($locationsList)) : [];
+  $hasOnline = false;
+  $physicalLocations = [];
+  foreach ($locs as $loc) {
+      $label = trim((string) $loc);
+      if ($label === '') { continue; }
+      if (strcasecmp($label, 'online') === 0) { $hasOnline = true; continue; }
+      $physicalLocations[] = $label;
+  }
+  $physicalCount = count($physicalLocations);
   $participantRange = $participantRange ?? null;
 @endphp
 <div class="wow-features">
@@ -9,10 +17,7 @@
       <div class="col">
         <div class="wow-feature">
           <div class="wow-icon-circle">
-            <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-              <path d="M20 21a8 8 0 0 0-16 0"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/></svg>
           </div>
           <b>{{ $participantRange['label'] }}</b><span>{{ $participantRange['suffix'] }}</span>
         </div>
@@ -21,11 +26,7 @@
     <div class="col">
       <div class="wow-feature">
         <div class="wow-icon-circle">
-          <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-            <path d="M8 2v3"></path><path d="M16 2v3"></path>
-            <rect x="3" y="5" width="18" height="17" rx="2"></rect>
-            <path d="M3 10h18"></path>
-          </svg>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z"/></svg>
         </div>
         <b>12 months</b><span>validity</span>
       </div>
@@ -33,23 +34,27 @@
     <div class="col">
       <div class="wow-feature">
         <div class="wow-icon-circle">
-          <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-            <path d="M12 21s7-4.4 7-11a7 7 0 0 0-14 0c0 6.6 7 11 7 11Z"></path>
-            <circle cx="12" cy="10" r="2"></circle>
-          </svg>
+          @if($hasOnline && $physicalCount === 0)
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M4.37 7.657c2.063.528 2.396 2.806 3.202 3.87 1.07 1.413 2.075 1.228 3.192 2.644 1.805 2.289 1.312 5.705 1.312 6.705M20 15h-1a4 4 0 0 0-4 4v1M8.587 3.992c0 .822.112 1.886 1.515 2.58 1.402.693 2.918.351 2.918 2.334 0 .276 0 2.008 1.972 2.008 2.026.031 2.026-1.678 2.026-2.008 0-.65.527-.9 1.177-.9H20M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>
+          @else
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"/></svg>
+          @endif
         </div>
-        <b><span class="text-decoration-underline">{{ $locCount }}</span></b><span>in the UK</span>
+        @if($hasOnline && $physicalCount === 0)
+          <b>Online</b><span>only</span>
+        @elseif($hasOnline && $physicalCount > 0)
+          <b>{{ $physicalCount }} {{ \Illuminate\Support\Str::plural('location', $physicalCount) }}</b><span>in the UK &amp; Online</span>
+        @elseif($physicalCount > 0)
+          <b>{{ $physicalCount }} {{ \Illuminate\Support\Str::plural('location', $physicalCount) }}</b><span>in the UK</span>
+        @else
+          <b>Flexible</b><span>format</span>
+        @endif
       </div>
     </div>
     <div class="col">
       <div class="wow-feature">
         <div class="wow-icon-circle">
-          <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-            <path d="M4 7h16"></path>
-            <path d="M7 4v6"></path>
-            <path d="M17 4v6"></path>
-            <path d="M5 21h14a2 2 0 0 0 2-2V9H3v10a2 2 0 0 0 2 2Z"></path>
-          </svg>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m11.5 11.5 2.071 1.994M4 10h5m11 0h-1.5M12 7V4M7 7V4m10 3V4m-7 13H8v-2l5.227-5.292a1.46 1.46 0 0 1 2.065 2.065L10 17Zm-5 3h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/></svg>
         </div>
         <b>Flexible</b><span>booking</span>
       </div>
@@ -57,11 +62,7 @@
     <div class="col">
       <div class="wow-feature">
         <div class="wow-icon-circle">
-          <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-            <path d="M3 12h18"></path>
-            <path d="M7 8l-4 4 4 4"></path>
-            <path d="M17 16l4-4-4-4"></path>
-          </svg>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/></svg>
         </div>
         <b>Easy</b><span>exchanges</span>
       </div>
@@ -69,10 +70,7 @@
     <div class="col">
       <div class="wow-feature">
         <div class="wow-icon-circle">
-          <svg viewBox="0 0 24 24" fill="none" stroke-width="2">
-            <path d="M4 4h16v16H4z"></path>
-            <path d="M22 6 12 13 2 6"></path>
-          </svg>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16v-5.5A3.5 3.5 0 0 0 7.5 7m3.5 9H4v-5.5A3.5 3.5 0 0 1 7.5 7m3.5 9v4M7.5 7H14m0 0V4h2.5M14 7v3m-3.5 6H20v-6a3 3 0 0 0-3-3m-2 9v4m-8-6.5h1"/></svg>
         </div>
         <b>Instant</b><span>delivery</span>
       </div>
